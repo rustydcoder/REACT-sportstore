a9d964291fd4fcf977d85a4cb4c5994474b254ca
Emmanuel George <rustydcoder@gmail.com>
2020-07-09 04:55:53 +0100

    added set up web server

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const chokidar = require('chokidar');
+const jsonServer = require('json-server');
+const cors = require('cors');
+
+const fileName = process.argv[2] || './data.js';
+const port = process.argv[3] || 3500;
+
+let router = undefined;
+
+const app = express()
+
+const createServer = () => {
+   delete require.cache[require.resolve(fileName)];
+
+   setTimeout(() => {
+      router = jsonServer.router(fileName.endsWith('.js') ? require(fileName) : fileName)
+   }, 100);
+}
+
+createServer()
+
+app.use(cors)
+app.use(jsonServer.bodyParser)
+app.use('/api', (req, res, next) => router(req, res, next))
+
+chokidar.watch(fileName).on('change', () => {
+   console.log('Reloading web service data...')
+   createServer()
+   console.log('Reloading web service data complete.')
+})
+
+app.listen(port, () => console.log(`Web service running on port ${port}`))
