3ba4efbd96da93ce497bb9598e704a827e20213a
Emmanuel George <rustydcoder@gmail.com>
2020-07-30 22:45:02 +0100

    extending datastore to handle cart actions

--- src/data/Types.js
+++ src/data/Types.js
@@ -4,7 +4,11 @@ const DataTypes = {
 }
 
 const ActionTypes = {
-   DATA_LOAD: 'data_load'
+   DATA_LOAD: 'data_load',
+   CART_ADD: 'cart_add',
+   CART_UPDATE: 'cart_update',
+   CART_REMOVE: 'cart_delete',
+   CART_CLEAR: 'cart_clear'
 }
 
 export { DataTypes, ActionTypes }
